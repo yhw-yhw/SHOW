1ba677c3d233e5e5e8a6383cb95e1f5d13d077aa
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2023-02-24 16:56:07 +0700

    Update model_smplx_config.py
    Fix "/"

--- configs/base/model_smplx_config.py
+++ configs/base/model_smplx_config.py
@@ -5,7 +5,7 @@ if 0:
     betas_ver_temp='{{ fileDirname }}/../../../data/id_pic/betas_shape2019male_exp2019/{}.npy'
 elif 0:
     smplx_model_path='{{ fileDirname }}/../../../models/smplx/SMPLX_NEUTRAL_2020_org.npz'
-    betas_ver_temp='{{ fileDirname }}/../../../data/id_pic/betas_shape2020natrual_exp2020/{}.npy'\
+    betas_ver_temp='{{ fileDirname }}/../../../data/id_pic/betas_shape2020natrual_exp2020/{}.npy'
 elif 1:
     smplx_model_path='{{ fileDirname }}/../../../models/smplx/SMPLX_MALE_shape2019_exp2020.npz'
     betas_ver_temp='{{ fileDirname }}/../../../data/id_pic/SMPLX_MALE_shape2019_exp2020/{}.npy'
